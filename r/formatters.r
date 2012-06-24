6065b71cf437068aba08be49e1f74135d309ccfb
jaredlander <info@jaredlander.com>
2012-06-24 18:53:38 -0400

    Formatter functions and their help files.

--- r/formatters.r
+++ r/formatters.r
@@ -41,19 +41,103 @@ multiple <- function(x, multiple=c("K", "M", "B", "T", "H", "k", "m", "b", "t", 
 
 #' multiple_format
 #' 
+#' Multiple Style Formatting
 #' 
+#' Since ggplot requires a function for formatting this allows the user to specify the function's arguments, which will return a function that can be used by ggplot.
+#' 
+#' @param \dots Arguments to be passed onto \code{\link{multiple}}
+#' @return The function \code{\link{multiple}}.
+#' @author Jared P. Lander
+#' @export multiple_format
+#' @aliases multiple_format
+#' @examples
+#' 
+#' vect <- c(1000, 1500, 23450, 21784, 875003780)
+#' multiple_format()(vect)
+#' multiple_format(extra=dollar)(vect)
+#' multiple_format(extra=identity)(vect)
 multiple_format <- function(...)
 {
     function(x) multiple(x, ...)
 }
 
 
+
+#' multiple.dollar
+#' 
+#' Order of Magnitude Formatter
+#' 
+#' Simply a wrapper for multiple that prespecifies the extra dollar.
+#' 
+#' @author Jared P. Lander
+#' @aliases multiple.dollar
+#' @export multiple.dollar
+#' @import scales
+#' @param x Vector of numbers to be formatted.
+#' @param The multiple to display numbers in.  This symbol will be added to the end of the numbers.
+#' @param digits Number of decimal places for rounding.
+#' @return Character vector of dollar formatted numbers.
+#' @examples
+#' vect <- c(1000, 1500, 23450, 21784, 875003780)
+#' multiple.dollar(vect)
+#' multiple.dollar(vect, multiple="k")
+#' multiple.dollar(vect, multiple="h")
+#'
 multiple.dollar <- function(x, multiple=c("K", "M", "B", "T", "k", "m", "b", "t"))
 {
     multiple(x=x, multiple=multiple, extra=dollar)
 }
 
+
+
+#' multiple.comma
+#' 
+#' Order of Magnitude Formatter
+#' 
+#' Simply a wrapper for multiple that prespecifies the extra comma.
+#' 
+#' @author Jared P. Lander
+#' @aliases multiple.comma
+#' @export multiple.comma
+#' @import scales
+#' @param x Vector of numbers to be formatted.
+#' @param The multiple to display numbers in.  This symbol will be added to the end of the numbers.
+#' @param digits Number of decimal places for rounding.
+#' @return Character vector of comma formatted numbers.
+#' @examples
+#' vect <- c(1000, 1500, 23450, 21784, 875003780)
+#' multiple.comma(vect)
+#' multiple.comma(vect, multiple="k")
+#' multiple.comma(vect, multiple="h")
+#'
 multiple.comma <- function(x, multiple=c("K", "M", "B", "T", "k", "m", "b", "t"))
 {
     multiple(x=x, multiple=multiple, extra=comma)
+}
+
+
+
+#' multiple.comma
+#' 
+#' Order of Magnitude Formatter
+#' 
+#' Simply a wrapper for multiple that prespecifies the extra identity.
+#' 
+#' @author Jared P. Lander
+#' @aliases multiple.identity
+#' @export multiple.identity
+#' @import scales
+#' @param x Vector of numbers to be formatted.
+#' @param The multiple to display numbers in.  This symbol will be added to the end of the numbers.
+#' @param digits Number of decimal places for rounding.
+#' @return Character vector of formatted numbers.
+#' @examples
+#' vect <- c(1000, 1500, 23450, 21784, 875003780)
+#' multiple.identity(vect)
+#' multiple.identity(vect, multiple="k")
+#' multiple.identity(vect, multiple="h")
+#'
+multiple.identity <- function(x, multiple=c("K", "M", "B", "T", "k", "m", "b", "t"))
+{
+    multiple(x=x, multiple=multiple, extra=identity)
 }
