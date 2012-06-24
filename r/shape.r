29ac5bf3d052779b8c72b816e4c7162245548f50
jaredlander <info@jaredlander.com>
2012-06-24 17:33:24 -0400

    Functions for dealing with shape files.

---
+++ r/shape.r
@@ -0,0 +1,57 @@
+#' prepare.shape.fortify
+#' 
+#' Prepare Shape File for Plotting
+#' 
+#' Takes in a shape object, fortifies it and joins in it's accompanying data.
+#' 
+#' @param shape A shape object as read using maptools.
+#' @return A \code{\link{data.frame}} suitable for use in \code{ggplot}.
+#' @author Jared P. Lander
+#' @aliases prepare.shape.fortify
+#' @import ggplot2 plyr stringr rgeos rgdal stringr
+#' @seealso prepare.shape
+#' @examples \dontrun{
+#' nyc <- readShapeSpatial(fn="nyct2010.shp")
+#' nyc.df <- prepare.shape.fortify(nyc)
+#' }
+#' 
+prepare.shape.fortify <- function(shape)
+{
+    # needed for fortify to work
+    suppressMessages(gpclibPermit())
+    # put rownames as id in the data
+    shape@data$id <- rownames(shape@data)
+    # fortify the shape file
+    shape.points <- fortify(shape, region="id")
+    # join in info from data
+    shape.df <- join(shape.points, shape@data, by="id")
+    #housekeeping
+    rm(shape, shape.points)
+    
+    return(shape.df)
+}
+
+
+#' prepare.shape
+#' 
+#' Prepare Shape File for Plotting
+#' 
+#' Reads in a shape file and then fortifies it.
+#' @author Jared P. Lander
+#' @seealso prepare.shape.fortify
+#' @import maptools
+#' @export prepare.shape
+#' @aliases prepare.shape
+#' @param file Name of shape file.
+#' @return A \code{\link{data.frame}} suitable for use in \code{ggplot}.
+#' @examples \dontrun{
+#' nyc.df <- prepare.shape("nyct2010.shp")
+#' }
+#' 
+prepare.shape <- function(file)
+{
+    # read shape file
+    shape <- readShapeSpatial(fn=file)
+    # frotify and join the file
+    prepare.shape.fortify(shape)
+}
