fa3c7be536fea580cbbd8c334554ddd8402b567d
jaredlander <info@jaredlander.com>
2012-06-25 01:07:26 -0400

    Adding options for faceting.

--- r/plotting.r
+++ r/plotting.r
@@ -10,7 +10,7 @@
 #' @author Jared P. Lander
 #' @aliases map.plot
 #' @export map.plot
-#' @import ggplot2
+#' @import ggplot2 useful
 #' @param data \code{\link{data.frame}} resulting from using \code{\link{fortify}} on a shapefile object.
 #' @param variable Character indicating which column should be plotted.
 #' @param fill.color.high Color to use for top of gradient.
@@ -33,13 +33,26 @@
 #' 
 map.plot <- function(data, variable, fill.color.high=muted("green"), space="Lab", path.color="white", title=NULL, 
                      xlab=NULL, ylab=NULL, barheight=15, formatter=multiple_format(multiple="K", extra=comma),
-                     legend.position=c("right", "bottom", "left", "top", "none"))
+                     legend.position=c("right", "bottom", "left", "top", "none"), 
+                     lhs=NULL, rhs=NULL, facet=c("none", "facet_wrap", "facet_grid"), wrap.nrow=NULL, wrap.ncol=NULL, 
+                     scales=c("fixed", "free", "free_y", "free_x")
+                     )
 {
     # generate a bunch of prebuilt map options for easier plotting
     mapOpts <- map.options()
     
     # get legend.position
     legend.position <- match.arg(legend.position)
+    
+    # get arguments about faceting
+    facet <- match.arg(facet)
+    scales <- match.arg(scales)
+    
+    # build the string to call the helper functions
+    facet <- sprintf("%s_helper", facet)
+    
+    # build facets, could be NULL or facet_wrap or facet_grid
+    facet <- do.call(facet, args=list(formula=build.formula(lhs=lhs, rhs=rhs), nrow=wrap.nrow, ncol=wrap.ncol, scales=scales))
     
     # start plotting
     p <- ggplot(data) + 
@@ -57,9 +70,104 @@ map.plot <- function(data, variable, fill.color.high=muted("green"), space="Lab"
         # make the color scale a gradient and use the chosen formatter
         scale_fill_gradient2(labels=formatter, space=space, high=fill.color.high) + 
         # title of plot
-        opts(title=title)
+        opts(title=title) +
+        # faceting if called for
+        facet
     
     return(p)
+}
+
+
+#' facet_wrap_helper
+#' 
+#' Call facet_wrap
+#' 
+#' This function merely serves to call \code{link{facet_wrap}}.  It is needed because the program doesn't know if \code{link{facet_wrap}} or \code{\link{facet_grid}} is being called and hence doesn't know what arguments to pass, and those functions are not equiped with \dots.
+#' 
+#' @author Jared P. Lander
+#' @aliases facet_wrap_helper facet_grid_helper none_helper
+#' @seealso facet_wrap facet_grid
+#' @import ggplot2
+#' @param formula \code{\link{formula}} for use in \code{link{facet_wrap}}
+#' @param nrow Number of rows.
+#' @param ncol Number of columns.
+#' @param scales Should scales be fixed ("fixed", the default), free ("free"), or free in one dimension ("free_x", "free_y").
+#' @param \dots Catch all.
+#' @return The result of \code{link{facet_wrap}}.
+#' 
+facet_wrap_helper <- function(formula, nrow=NULL, ncol=NULL, 
+                              scales=c("fixed", "free", "free_y", "free_x"), ...)
+{
+    scales <- match.arg(scales)
+    
+    facet_wrap(formula, nrow=nrow, ncol=ncol, scales=scales)
+}
+
+
+#' facet_grid_helper
+#' 
+#' Call facet_grid
+#' 
+#' This function merely serves to call \code{link{facet_grid}}.  It is needed because the program doesn't know if \code{link{facet_wrap}} or \code{\link{facet_grid}} is being called and hence doesn't know what arguments to pass, and those functions are not equiped with \dots.
+#' 
+#' @author Jared P. Lander
+#' @aliases facet_wrap_helper facet_grid_helper none_helper
+#' @seealso facet_wrap facet_grid
+#' @import ggplot2
+#' @param formula \code{\link{formula}} for use in \code{link{facet_grid}}
+#' @param nrow Number of rows.
+#' @param ncol Number of columns.
+#' @param scales Should scales be fixed ("fixed", the default), free ("free"), or free in one dimension ("free_x", "free_y").
+#' @param \dots Catch all.
+#' @return The result of \code{link{facet_grid}}.
+#'
+facet_grid_helper <- function(formula, 
+                              scales=c("fixed", "free", "free_y", "free_x"), ...)
+{
+    scales <- match.arg(scales)
+    
+    facet_grid(formula, scales=scales)
+}
+
+
+#' none_helper
+#' 
+#' Call none
+#' 
+#' This function merely serves to call \code{link{none}}.  It is needed because the program doesn't know if \code{link{facet_wrap}} or \code{\link{facet_grid}} is being called and hence doesn't know what arguments to pass, and those functions are not equiped with \dots.
+#' 
+#' @author Jared P. Lander
+#' @aliases facet_wrap_helper facet_grid_helper none_helper
+#' @seealso facet_wrap facet_grid none
+#' @import ggplot2
+#' @param \dots Catch all.
+#' @return \code{\link{NULL}}.
+#'
+none_helper <- function(...)
+{
+    none(...)
+}
+
+#' none
+#' 
+#' Just Null
+#' 
+#' Returns NULL regardless of what is put in.
+#' 
+#' @author Jared P. Lander
+#' @aliases none
+#' @export none
+#' @param \dots Anything.
+#' @return \code{\link{NULL}} regardless of inputs.
+#' @examples
+#' 
+#' none()
+#' none(1)
+#' none(mean)
+#' 
+none <- function(...)
+{
+    return(NULL)
 }
 
 #' map.options
